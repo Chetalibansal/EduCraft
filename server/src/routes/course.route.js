0366229a7b0747a37f73d46902c2afd594c14c1e
Chetalibansal <chetalibansal316@gmail.com>
2025-07-04 00:04:18 +0530

    fix: import createCourse controller in course route

--- server/src/routes/course.route.js
+++ server/src/routes/course.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import isAuthenticated from '../middlewares/isAuthenticated.js';
+import { createCourse } from '../controllers/course.controller.js';
 
 const router = express.Router();
 
